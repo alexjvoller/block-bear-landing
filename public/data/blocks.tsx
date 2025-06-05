91ba799df620a5eed361260ff7728daccfad8ada
Alex Voller <alexjvoller@gmail.com>
2025-06-06 06:37:46 +1000

    feat(chart): add data

---
+++ public/data/blocks.tsx
@@ -0,0 +1,37 @@
+export const blocks = [
+  {
+    name: "Q1 Cut",
+    slug: "q1-cut",
+    scheduledStart: new Date("2024-03-11"),
+    length: 54,
+    lengthInterval: "DAY",
+  },
+  {
+    name: "Q2 bulk",
+    slug: "q2-bulk",
+    scheduledStart: new Date("2024-05-05"),
+    length: 48,
+    lengthInterval: "DAY",
+  },
+  {
+    name: "INJURED!",
+    slug: "injury-cut",
+    scheduledStart: new Date("2024-08-12"),
+    length: 19,
+    lengthInterval: "DAY",
+  },
+  {
+    name: "permabulk",
+    slug: "permabulk",
+    scheduledStart: new Date("2024-09-02"),
+    length: 240,
+    lengthInterval: "DAY",
+  },
+  {
+    name: "Q2 mini-cut",
+    slug: "q2-mini-cut",
+    scheduledStart: new Date("2025-04-31"),
+    length: 21,
+    lengthInterval: "DAY",
+  },
+];
